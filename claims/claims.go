e0ab0c091af15acabd1c1ec8562731037d36abf6
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2019-11-19 14:08:16 +0000

    Change structure of claims JSON.

--- claims/claims.go
+++ claims/claims.go
@@ -13,12 +13,17 @@ const pkg = "github.com/sorsby/gin-rating-api/claims"
 // Getter gets the claims from the context.
 type Getter func(ctx context.Context) (claims Claims, ok bool, err error)
 
-// Claims is the authorizer claims from the API gateway proxy request
+// Claims represents the cognito user pool claims.
 type Claims struct {
-	Sub      string   `json:"sub"`
-	Groups   []string `json:"cognito:groups"`
-	Username string   `json:"cognito:username"`
-	Email    string   `json:"email"`
+	CognitoGroups   string `json:"cognito:groups"`
+	CognitoUsername string `json:"cognito:username"`
+	Email           string `json:"email"`
+	Sub             string `json:"sub"`
+}
+
+// Authorizer is the outer authorization object in the request context.
+type Authorizer struct {
+	Claims Claims `json:"claims"`
 }
 
 // Get gets the claims from the context.
@@ -28,16 +33,14 @@ func Get(ctx context.Context) (claims Claims, ok bool, err error) {
 		logger.Entry(pkg, "Post").Error("failed to proxy request from context")
 		return
 	}
-	logger.Entry(pkg, "claims.Get").WithField("authorizer", proxyReq.RequestContext.Authorizer).Info("got authorizer from request context")
-	logger.Entry(pkg, "claims.Get").WithField("claims", proxyReq.RequestContext.Authorizer["claims"]).Info("got claims json from request context")
 	claimsJSON, ok := proxyReq.RequestContext.Authorizer["claims"].([]byte)
+	if !ok {
+		logger.Entry(pkg, "Post").Error("expected json but was unable to assert type")
+		return
+	}
 	err = json.Unmarshal(claimsJSON, &claims)
 	if err != nil {
 		logger.Entry(pkg, "claims.Get").WithError(err).Error("failed to unmarshal claims")
-		return
-	}
-	if !ok {
-		logger.Entry(pkg, "claims.Get").Error("failed to extract claims from request context")
 		return
 	}
 	return claims, true, err
