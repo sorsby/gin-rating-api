afeb00a217c26b4a132c3809bc9fdd0cc07c77ef
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2019-11-19 12:15:02 +0000

    Unmarshal claims json.

--- claims/claims.go
+++ claims/claims.go
@@ -2,6 +2,7 @@ package claims
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/akrylysov/algnhsa"
 	"github.com/sorsby/gin-rating-api/logger"
@@ -14,22 +15,28 @@ type Getter func(ctx context.Context) (claims Claims, ok bool)
 
 // Claims is the authorizer claims from the API gateway proxy request
 type Claims struct {
-	Sub    string   `json:"sub"`
-	Groups []string `json:"groups"`
+	Sub      string   `json:"sub"`
+	Groups   []string `json:"cognito:groups"`
+	Username string   `json:"cognito:username"`
+	Email    string   `json:"email"`
 }
 
 // Get gets the claims from the context.
-func Get(ctx context.Context) (claims Claims, ok bool) {
+func Get(ctx context.Context) (claims Claims, ok bool, err error) {
 	proxyReq, ok := algnhsa.ProxyRequestFromContext(ctx)
 	if !ok {
 		logger.Entry(pkg, "Post").Error("failed to proxy request from context")
 		return
 	}
-	logger.Entry(pkg, "claims.Get").WithField("proxyReq", proxyReq).Info("parsing claims")
-	claims, ok = proxyReq.RequestContext.Authorizer["claims"].(Claims)
-	if !ok {
-		logger.Entry(pkg, "Post").Error("failed to extract claims from request context")
+	claimsJSON, ok := proxyReq.RequestContext.Authorizer["claims"].([]byte)
+	err = json.Unmarshal(claimsJSON, &claims)
+	if err != nil {
+		logger.Entry(pkg, "claims.Get").WithError(err).Error("failed to unmarshal claims")
 		return
 	}
-	return claims, true
+	if !ok {
+		logger.Entry(pkg, "claims.Get").Error("failed to extract claims from request context")
+		return
+	}
+	return claims, true, err
 }
